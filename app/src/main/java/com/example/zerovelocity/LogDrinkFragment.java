842afe566068e671f2ae3cd879ffe4f089283a46
proche13 <23379707@studentmail.ul.ie>
2026-03-26 00:25:13 +0000

    Populated Spinner in  LogDrinkFragment,java

--- app/src/main/java/com/example/zerovelocity/LogDrinkFragment.java
+++ app/src/main/java/com/example/zerovelocity/LogDrinkFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -28,6 +29,10 @@ public class LogDrinkFragment extends Fragment {
         categorySpinner = view.findViewById(R.id.spinnerCategory);
         unitsEditText = view.findViewById(R.id.editTextUnits);
         logButton = view.findViewById(R.id.buttonLog);
+
+        ArrayAdapter<LogEntry.Category> adapter = new ArrayAdapter<>(requireContext(),android.R.layout.simple_spinner_item, LogEntry.Category.values());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(adapter);
 
         viewModel = new ViewModelProvider(this).get(LogDrinkViewModel.class);
 
